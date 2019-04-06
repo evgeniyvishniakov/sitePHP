2630ec123e48fc1b77c410faed37413db1a61846
evgeniyvishniakov <39340302+evgeniyvishniakov@users.noreply.github.com>
2019-04-06 12:34:49 +0300

    footer commit

--- Showroom/js/main.js
+++ Showroom/js/main.js
@@ -1,7 +1,14 @@
-$('.link').hover(function() {
-    $(this).next().show(); // получаем следующий за данным .link элемент (т.е. span)
-}, 
-function() {
-    $(this).next().hide();
+$(document).ready(function(){
+    $('.header_bottom_menu ul li a').hover(
+        function(){ 
+            $(this).find('.header_bottom_menu ul li a').toggleClass('drop_active');
+        }
+    );
 });
 
+$('.item_menu').mouseover(function(){
+    $(this).addClass('drop_active');
+    });
+    $('.item_menu').mouseleave(function(){
+    $(this).removeClass('drop_active');
+    });

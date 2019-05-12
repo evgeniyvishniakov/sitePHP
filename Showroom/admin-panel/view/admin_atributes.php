5f622c7002ad034406fdfe15f257c3801bf16079
evgeniyvishniakov <39340302+evgeniyvishniakov@users.noreply.github.com>
2019-05-12 22:13:29 +0300

    admin_atributes

--- Showroom/admin-panel/view/admin_atributes.php
+++ Showroom/admin-panel/view/admin_atributes.php
@@ -4,29 +4,51 @@ include_once './class/class_admin_colors.php';
 include_once './class/class_admin_sizes.php';
 
 
+
 $admim_brands = new Admin_Brands;
-$admim_colors = new Admin_Colors;
 $admim_sizes = new Admin_Sizes;
+$admim_colors = new Admin_Colors;
+$name = $_POST['name'];
+$foto = $_POST['foto'];
 
+if (!empty($_POST["atribute"]) AND empty($_POST["ok_atribute"])) {
+  
+  switch ($_POST["atribute"]) {
 
+    case 'brand':
+      $admim_brands->brands_add($name, $foto);
+        break;
+    case 'size':
+      $admim_sizes = new Admin_Sizes;
+        break;
+    case 'color':
+      $admim_colors = new Admin_Colors;
+        break;
+
+  }
+}else {
+  $error_add = 'Выберите атрибут';
+}
+echo '<pre>';
+var_dump($_POST);
+echo '</pre>';
 ?>
 
 
 
 <div class="table_input">
         <h3>Добавить Бренд</h3>
-
-        
+        <p><?php echo $error_add; ?></p>
         <form action="#" method="POST">
         <p>Выберите атрибут:</p><select  name="atribute">
         <option selected disabled>Выбрать</option>
-        <option value="1">Бренд</option>
-        <option value="2">Размер</option>
-        <option value="3">Цвет</option>
+        <option value="brand">Бренд</option>
+        <option value="size">Размер</option>
+        <option value="color">Цвет</option>
         </select>
 
-          <p>Название:</p><input type="text" name="name_brend">
-          <p>Логотип:</p><input type="file">
+          <p>Название:</p><input type="text" name="name">
+          <p>Логотип:</p><input type="file" name ="foto">
           <input type="submit" name="ok_atribute" value="Сохранить">
         </form>
       </div>
@@ -43,6 +65,7 @@ $admim_sizes = new Admin_Sizes;
             <tr>
               <td><?php echo $value['foto'];?></td>
               <td><?php echo $value['name'];?></td>
+              <td><a class="edit" href="?get=products&edit=<?php echo $value['id']; ?>"><i class="fa fa-pencil-square-o"></i></a></td>
               <td><a class="del" href="?get=atribute&brand_del=<?php echo $value['id'];?>"><i class="fa fa-trash"></i></a></td>
             </tr>
             <?php  } ?>
@@ -60,6 +83,7 @@ $admim_sizes = new Admin_Sizes;
             <tr>
               <td><?php echo $value['id'];?></td>
               <td><?php echo $value['name'];?></td>
+              <td><a class="edit" href="?get=products&edit=<?php echo $value['id']; ?>"><i class="fa fa-pencil-square-o"></i></a></td>
               <td><a class="del" href="?get=atribute&color_del=<?php echo $value['id'];?>"><i class="fa fa-trash"></i></a></td>
             </tr>
             <?php  } ?>
@@ -77,6 +101,7 @@ $admim_sizes = new Admin_Sizes;
             <tr>
               <td><?php echo $value['id'];?></td>
               <td><?php echo $value['name'];?></td>
+              <td><a class="edit" href="?get=products&edit=<?php echo $value['id']; ?>"><i class="fa fa-pencil-square-o"></i></a></td>
               <td><a class="del" href="?get=atribute&size_del=<?php echo $value['id'];?>"><i class="fa fa-trash"></i></a></td>
             </tr>
             <?php  } ?>
